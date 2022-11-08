140497a01c6a2288568f1d65b6e0302ba0444d32
UhaiMong <uhaimarma2016@gmail.com>
2022-11-08 20:55:54 +0600

    header design

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shaired/Footer/Footer';
+import Header from '../Pages/Shaired/Header/Header';
 
 const Main = () => {
     return (
         <div>
-            
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Routers/Router/Router.js
+++ src/Routers/Router/Router.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Home from "../../Pages/Home/Home";
 
-const Router = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default Router;
+export const routers = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element:<Home></Home>
+            },
+        ]
+    },
+    {
+        path: '*',
+        element: <p className="text5xl font-extrabold text-center">The page not found !! 404 page</p>
+    }
+])
