d9a2cf48dd2d4fc8244f07fb1f878c3d25c805b8
UhaiMong <uhaimarma2016@gmail.com>
2022-11-10 23:41:20 +0600

    update 3

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Audience from '../Audience/Audience';
 import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
+import SpecialOffer from '../SpecialOffer/SpecialOffer';
 const Home = () => {
     return (
         <>
             <Banner></Banner>
             <Services></Services>
+            <SpecialOffer></SpecialOffer>
             <Audience></Audience>
         </>
 
